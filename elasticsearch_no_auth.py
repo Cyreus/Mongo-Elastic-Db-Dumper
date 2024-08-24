49f165e972cab89fcb010889a6239084ea39a8dd
Muhammed Ali Öztürk <Muhammed_Ali95@outlook.com.tr>
2024-08-24 17:17:30 +0300

    Update elasticsearch_no_auth.py

--- elasticsearch_no_auth.py
+++ elasticsearch_no_auth.py
@@ -7,7 +7,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class ElasticSearchNoAuth:
     def __init__(self):
-        self.api = Shodan('civYo8aALAhkpfQ3azo7Hw9BsltftueC')
+        self.api = Shodan('')
         self.limit = 0
         self.es_urls = []
 
